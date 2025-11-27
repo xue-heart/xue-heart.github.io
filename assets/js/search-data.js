d23719ed623a5195f0d241bc01a0dcf8c350b0a7
xue-heart <xwolfs@hotmail.com>
2025-11-27 08:29:05 +0000

    Deploying to gh-pages from @ xue-heart/xue-heart.github.io@9128139ec38b7856b62b74ec1656eeaf28e6961f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -451,28 +451,28 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
+          section: "News",},{id: "projects-echoone",
+          title: 'EchoOne',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
+            },},{id: "projects-echo3d",
+          title: 'Echo3D',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
+            },},{id: "projects-clas",
+          title: 'CLAS',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
+            },},{id: "projects-motion",
+          title: 'Motion',
           description: "another without an image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
+            },},{id: "projects-wall-thickness",
+          title: 'Wall Thickness',
           description: "a project with a background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
